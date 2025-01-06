dc5eda5c577cd7c8224ea1ed55d998d04aab430b
Bhavya1815 <bhavyarajraulji1882@gmail.com>
2025-01-06 12:12:18 +0530

    Billing-System

---
+++ OverStore-master/src/Components/About.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+const About = () => {
+  return (
+    <>
+      <header id="home" className="header">
+        <section className="section pt-0" id="about">
+          <div className="container text-center">
+            <div className="about">
+              <div className="about-img-holder" style={{ paddingTop: "30px" }}>
+                <img
+                  src={require("../assets/imgs/man.png")}
+                  className="about-img"
+                  alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page"
+                />
+              </div>
+              <div className="about-caption">
+                <p className="section-subtitle text-primary font-weight-bold">
+                  Who Am I ?
+                </p>
+                <h2 className="section-title mb-3">About Me</h2>
+                <p className="text-primary font-weight-bold" style={{overflow:"hidden"}}>
+                Welcome to OverStore!
+We're your one-stop shop for simplifying your daily essentials! We understand how busy life can get, and that's why we offer a wide range of personal care, food, and cleaning products all under one roof.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </header>
+    </>
+  );
+};
+export default About;
